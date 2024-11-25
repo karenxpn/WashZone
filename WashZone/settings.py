b53173e8508ae7e05657c28c6fd58bd715e2762c
karenxpn <karenmirakyan@gmail.com>
2024-11-26 02:10:50 +0400

    postgres integration

--- WashZone/settings.py
+++ WashZone/settings.py
@@ -83,10 +83,21 @@ WSGI_APPLICATION = 'WashZone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),  # Default name unless changed
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
