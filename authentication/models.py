a035d1f85838d0911f9342d05ef90ace865c100e
karenxpn <karenmirakyan@gmail.com>
2024-11-25 12:05:42 +0400

    user model and phone otp models created

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,23 @@
+import random
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=15, unique=True, null=True, blank=True)
+    is_phone_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
+
+
+class PhoneOTP(models.Model):
+    phone_number = models.CharField(max_length=15, unique=True)
+    otp = models.CharField(max_length=6, blank=True, null=True)
+    is_verified = models.BooleanField(default=False)
+
+    def generate_otp(self):
+        self.otp = f"{random.randint(100000, 999999)}"
+        self.save()
+
