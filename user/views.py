99f679ab024e4cabbd6b300b9fcd8a6ed1f34e2f
karenxpn <karenmirakyan@gmail.com>
2024-11-28 15:41:43 +0400

    user serializer

--- user/views.py
+++ user/views.py
@@ -2,15 +2,16 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from user.serializers.user_serializer import UserSerializer
+
+
 # Create your views here.
 class UserDetailView(APIView):
     permission_classes = [IsAuthenticated]
 
-    def get(self, request):
-        return Response({
-            'phone': request.user.phone_number,
-            'email': 'the email here'
-        })
+    def get(self, request, *args, **kwargs):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
 
     def post(self, request):
         return Response({
