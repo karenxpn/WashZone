dd0061e2b2450e23f87d346a908d8474746675eb
karenxpn <karenmirakyan@gmail.com>
2024-11-28 17:48:47 +0400

    update user info

---
+++ user/serializers/update_user_serializer.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from user.models import User
+import re
+
+
+
+class UpdateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'email_promotions_enabled',
+            'first_name',
+            'last_name',
+            'notifications_enabled'
+        )
+
+    # validations
+    def validate_email(self, email):
+        valid = re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email)
+
+        if not valid:
+            raise serializers.ValidationError('Invalid email address')
+        return email
+
+    def validate_first_name(self, first_name):
+        if first_name is not None:
+            if len(first_name) < 3:
+                raise serializers.ValidationError('First name must be at least 3 characters')
+
+        return first_name
+
+    def validate_last_name(self, last_name):
+        if last_name is not None:
+            if len(last_name) < 3:
+                raise serializers.ValidationError('Last name must be at least 3 characters')
+
+        return last_name
--- user/views.py
+++ user/views.py
@@ -1,7 +1,10 @@
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from authentication.decorators import validate_request, validate_unexpected_fields
+from user.serializers.update_user_serializer import UpdateUserSerializer
 from user.serializers.user_serializer import UserSerializer
 
 
@@ -13,7 +16,14 @@ class UserDetailView(APIView):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
 
+    @validate_unexpected_fields(UpdateUserSerializer)
+    @validate_request(UpdateUserSerializer)
     def post(self, request):
+        serializer = UpdateUserSerializer(request.user, data=request.data, partial=True)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer.save()
         return Response({
-            'request': 'post request',
-        })
+            'message': 'Profile updated successfully',
+        }, status=status.HTTP_200_OK)
