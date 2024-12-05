8bf3a4abffc9005f7b1e6f3c6405a09b76ab13b4
karenxpn <karenmirakyan@gmail.com>
2024-12-05 15:09:21 +0400

    created an intermediate table for service and feature many-to-many relationship

--- services/service_models/feature.py
+++ services/service_models/feature.py
@@ -1,6 +1,8 @@
 # features that the service includes or can be added
 from django.db import models
 
+from services.service_models.service import Service
+
 
 class Feature(models.Model):
     name = models.CharField(max_length=100)
@@ -13,4 +15,22 @@ class Feature(models.Model):
         return self.name
 
 
-# intermediate model for the feature and provider ( many-to-many relationship)
+# intermediate model for the feature and service ( many-to-many relationship)
+
+class ServiceFeature(models.Model):
+    service = models.ForeignKey(Service, on_delete=models.CASCADE, related_name='features')
+    feature = models.ForeignKey(Feature, on_delete=models.CASCADE, related_name='services')
+    is_included = models.BooleanField(default=False)
+    extra_cost = models.DecimalField(
+        max_digits=10, decimal_places=2, default=0,
+        help_text="Extra cost for the feature if it's optional"
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('service', 'feature')
+
+    def __str__(self):
+        return f"{self.service.name} - {self.feature.name}"
