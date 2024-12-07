6cdcc576046e7abfec90900929131d8a671313dd
karenxpn <karenmirakyan@gmail.com>
2024-12-07 16:57:00 +0400

    update feature bug fix

--- services/serializers/service_feature_serializer.py
+++ services/serializers/service_feature_serializer.py
@@ -4,12 +4,13 @@ from services.models import ServiceFeature
 class ServiceFeatureSerializer(serializers.ModelSerializer):
     feature_name = serializers.CharField(source='feature.name', read_only=True)
     description = serializers.CharField(source='feature.description')
-    id = serializers.IntegerField(source='feature.id', read_only=True)
+    feature_id = serializers.IntegerField(source='feature.id', read_only=True)
 
     class Meta:
         model = ServiceFeature
         fields = (
             'id',
+            'feature_id',
             'is_included',
             'feature_name',
             'description',
--- services/service_views/feature.py
+++ services/service_views/feature.py
@@ -1,12 +1,11 @@
-from rest_framework import viewsets, permissions, status
+from rest_framework import viewsets, status
 from rest_framework.exceptions import NotFound
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from authentication.decorators import validate_request
 from services.serializers.feature_serializer import FeatureSerializer, FeatureUpdateSerializer
-from services.serializers.service_feature_serializer import ServiceFeatureSerializer
-from services.service_models.feature import Feature, ServiceFeature
+from services.service_models.feature import Feature
 
 
 class FeatureViewSet(viewsets.ModelViewSet):
@@ -22,11 +21,11 @@ class FeatureViewSet(viewsets.ModelViewSet):
 
     @validate_request(FeatureUpdateSerializer)
     def update(self, request, *args, **kwargs):
-        return update_feature(self, request, *args, **kwargs)
+        return super().update(request, *args, **kwargs)
 
     @validate_request(FeatureUpdateSerializer)
     def partial_update(self, request, *args, **kwargs):
-        return update_feature(self, request, *args, **kwargs)
+        return super().partial_update(request, *args, **kwargs)
 
     def destroy(self, request, *args, **kwargs):
         try:
@@ -35,29 +34,3 @@ class FeatureViewSet(viewsets.ModelViewSet):
             return Response({"message": "Feature deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
         except NotFound:
             return Response({"message": "Feature not found"}, status=status.HTTP_404_NOT_FOUND)
-
-
-def update_feature(self, request, *args, **kwargs):
-    partial = kwargs.pop('partial', False)
-    instance = self.get_object()
-    serializer = self.get_serializer(instance, data=request.data, partial=partial)
-    serializer.is_valid(raise_exception=True)
-    feature = serializer.save()
-
-    # Propagate changes to related ServiceFeature records
-    related_service_features = ServiceFeature.objects.filter(feature=feature)
-    for service_feature in related_service_features:
-        # Use the ServiceFeatureSerializer to validate and save changes
-        service_feature_data = {
-            'service': service_feature.service.id,
-            'feature': service_feature.feature.id,
-            'is_included': service_feature.is_included,
-            'extra_cost': feature.cost,
-        }
-        sf_serializer = ServiceFeatureSerializer(service_feature, data=service_feature_data, partial=True)
-        if sf_serializer.is_valid(raise_exception=True):
-            sf_serializer.save()
-
-    return Response(serializer.data)
-
-
