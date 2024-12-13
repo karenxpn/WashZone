ae130c7a0b06cae2d4573c1a6dffafee517ee3c9
karenxpn <karenmirakyan@gmail.com>
2024-12-13 16:47:24 +0400

    user in order fix

--- orders/order_models/order.py
+++ orders/order_models/order.py
@@ -1,9 +1,10 @@
 from django.db import models
 from services.service_models.provider import Provider
+from user.models import User
 
 
 class Order(models.Model):
-    user = models.ForeignKey("user.User", on_delete=models.CASCADE, related_name="orders")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
     provider = models.ForeignKey(Provider, on_delete=models.CASCADE, related_name="orders")
     status = models.CharField(max_length=20, choices=[
         ('pending', 'Pending'),
