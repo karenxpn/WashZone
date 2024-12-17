f87917a79407bdb3fd714e12e38424a8919fe801
karenxpn <karenmirakyan@gmail.com>
2024-12-17 23:00:45 +0400

    try except block in create order item feature serializer

--- orders/serializers/create/create_order_item_feature_serializer.py
+++ orders/serializers/create/create_order_item_feature_serializer.py
@@ -1,4 +1,6 @@
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from orders.order_models.order_item import OrderItem
 from orders.order_models.order_item_feature import OrderItemFeature
@@ -25,23 +27,45 @@ class CreateOrderItemFeatureSerializer(serializers.ModelSerializer):
         return representation
 
     def create(self, validated_data):
-        feature = validated_data['feature']
-        order_item_pk = self.context.get('order_item')
+        try:
+            feature = validated_data['feature']
+            order_item_pk = self.context.get('order_item')
 
-        order_item = OrderItem.objects.select_related('service').get(pk=order_item_pk)
-        service = order_item.service
+            try:
+                order_item = OrderItem.objects.select_related('service').get(pk=order_item_pk)
+                service = order_item.service
+            except ObjectDoesNotExist:
+                raise ValidationError("Invalid order item")
 
-        service_feature = ServiceFeature.objects.filter(
-            service=service,
-            feature=feature.id,
-        ).first()
+            try:
+                # Find service feature
+                service_feature = ServiceFeature.objects.filter(
+                    service=service,
+                    feature=feature.id,
+                ).first()
 
-        validated_data['extra_cost'] = service_feature.extra_cost if service_feature and not service_feature.is_included else 0
+                # Calculate extra cost
+                validated_data['extra_cost'] = (
+                    service_feature.extra_cost
+                    if service_feature and not service_feature.is_included
+                    else 0
+                )
+            except Exception:
+                validated_data['extra_cost'] = 0
 
-        order_item_feature = OrderItemFeature.objects.create(
-            order_item=order_item,
-            feature=feature,
-            extra_cost=validated_data['extra_cost'],
-        )
+            try:
+                # Create order item feature
+                order_item_feature = OrderItemFeature.objects.create(
+                    order_item=order_item,
+                    feature=feature,
+                    extra_cost=validated_data['extra_cost'],
+                )
+            except Exception:
+                raise ValidationError("Failed to create order item feature")
 
-        return order_item_feature
+            return order_item_feature
+
+        except ValidationError:
+            raise
+        except Exception:
+            raise ValidationError("Failed to create order item feature")
