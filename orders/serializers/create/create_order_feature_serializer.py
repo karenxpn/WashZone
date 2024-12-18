127d9d781e6b9928a8633eefc8cd139c2adefa68
karenxpn <karenmirakyan@gmail.com>
2024-12-18 13:37:07 +0400

    create order serializers

--- orders/serializers/create/create_order_feature_serializer.py
+++ orders/serializers/create/create_order_feature_serializer.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from orders.order_models.order_feature import OrderFeature
+from services.service_models.feature import Feature
+
 
 class CreateOrderFeatureSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderFeature
-        fields = ['feature', 'extra_cost']
+        fields = ['feature']
+
+    def validate_feature(self, value):
+        if not Feature.objects.filter(id=value.id).exists():
+            raise serializers.ValidationError("Feature does not exist.")
+        return value
--- orders/serializers/create/create_order_serializer.py
+++ orders/serializers/create/create_order_serializer.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 from orders.order_models.order import Order
+from orders.order_models.order_feature import OrderFeature
 from orders.serializers.create.create_order_feature_serializer import CreateOrderFeatureSerializer
+from services.service_models.feature import ServiceFeature
 
 
 class CreateOrderSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.phone_number')
-    features = CreateOrderFeatureSerializer(many=True, read_only=True)
+    features = CreateOrderFeatureSerializer(many=True, write_only=True)
 
     class Meta:
         model = Order
@@ -19,4 +21,31 @@ class CreateOrderSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError('The service does not belong to the specified provider.')
 
         return data
+
+
+    def create(self, validated_data):
+        features_data = validated_data.pop('features', [])  # Extract features data
+        order = Order.objects.create(**validated_data)
+        service = order.service
+
+        order_features = []
+        for feature_data in features_data:
+            feature = feature_data['feature']
+
+            service_feature = ServiceFeature.objects.filter(
+                service=service,
+                feature=feature.id
+            ).first()
+
+            extra_cost = (
+                service_feature.extra_cost
+                if service_feature and not service_feature.is_included
+                else 0
+            )
+
+            order_features.append(OrderFeature(order=order, feature=feature, extra_cost=extra_cost))
+
+        OrderFeature.objects.bulk_create(order_features)
+
+        return order
 
