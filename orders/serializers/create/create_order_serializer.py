8ccc8541690f77b260501c59fd39060d1077e866
karenxpn <karenmirakyan@gmail.com>
2024-12-19 22:06:04 +0400

    validate time slot separate file created

--- orders/serializers/create/create_order_serializer.py
+++ orders/serializers/create/create_order_serializer.py
@@ -4,6 +4,7 @@ from orders.order_models.order import Order
 from orders.order_models.order_feature import OrderFeature
 from orders.order_models.time_slot import TimeSlot
 from orders.serializers.create.create_order_feature_serializer import CreateOrderFeatureSerializer
+from orders.serializers.validate_time_slot import validate_time_slot
 from services.service_models.feature import ServiceFeature
 
 
@@ -27,60 +28,11 @@ class CreateOrderSerializer(serializers.ModelSerializer):
     def validate(self, data):
         service = data.get('service')
         provider = data.get('provider')
-        start_time = data.get('start_time')
-        end_time = data.get('end_time')
-        features = data.get('features', [])
-
-        if start_time <= timezone.now():
-            raise serializers.ValidationError('The start time must be in the future.')
 
         if service.provider != provider:
             raise serializers.ValidationError('The service does not belong to the specified provider.')
 
-
-        total_duration = service.duration_in_minutes
-        for feature_data in features:
-            feature = feature_data.get('feature')
-            service_feature = ServiceFeature.objects.get(service=service, feature=feature)
-
-            if service_feature and not service_feature.is_included:
-                total_duration += service_feature.extra_time_in_minutes
-
-
-        # validate the provided duration matches the actual duration
-        requested_duration = (end_time - start_time).total_seconds() / 60
-        if requested_duration != total_duration:
-            raise serializers.ValidationError('The requested time slot duration does not match the service duration.')
-
-        # validate the provided start and end hours match providers working hours
-        working_hours = provider.working_hours.filter(weekday=start_time.weekday())
-        valid_slot = False
-
-        for hours in working_hours:
-            working_start = start_time.replace(hour=hours.opening_time.hour, minute=hours.opening_time.minute)
-            working_end = start_time.replace(hour=hours.closing_time.hour, minute=hours.closing_time.minute)
-
-            if working_start <= start_time and end_time <= working_end:
-                valid_slot = True
-                break
-
-        if not valid_slot:
-            raise serializers.ValidationError('The requested time slot is outside the provider\'s working hours.')
-
-        special_closures = provider.special_closures.filter(date=start_time.date())
-
-        if special_closures.exists():
-            raise serializers.ValidationError('The provider is closed this day.')
-
-        overlapping_slots = TimeSlot.objects.filter(
-            provider=provider,
-            is_available=False,
-            start_time__lt=end_time,
-            end_time__gt=start_time,
-        )
-
-        if overlapping_slots.exists():
-            raise serializers.ValidationError('The requested time slot is not available.')
+        validate_time_slot(provider, service, data)
 
         return data
 
---
+++ orders/serializers/validate_time_slot.py
@@ -0,0 +1,57 @@
+from django.utils import timezone
+from rest_framework import serializers
+
+from orders.order_models.time_slot import TimeSlot
+from services.service_models.feature import ServiceFeature
+
+
+def validate_time_slot(provider, service, data):
+    features = data.get('features', [])
+    start_time = data.get('start_time')
+    end_time = data.get('end_time')
+
+    if start_time <= timezone.now():
+        raise serializers.ValidationError('The start time must be in the future.')
+
+    total_duration = service.duration_in_minutes
+    for feature_data in features:
+        feature = feature_data.get('feature')
+        service_feature = ServiceFeature.objects.get(service=service, feature=feature)
+
+        if service_feature and not service_feature.is_included:
+            total_duration += service_feature.extra_time_in_minutes
+
+    # validate the provided duration matches the actual duration
+    requested_duration = (end_time - start_time).total_seconds() / 60
+    if requested_duration != total_duration:
+        raise serializers.ValidationError('The requested time slot duration does not match the service duration.')
+
+    # validate the provided start and end hours match providers working hours
+    working_hours = provider.working_hours.filter(weekday=start_time.weekday())
+    valid_slot = False
+
+    for hours in working_hours:
+        working_start = start_time.replace(hour=hours.opening_time.hour, minute=hours.opening_time.minute)
+        working_end = start_time.replace(hour=hours.closing_time.hour, minute=hours.closing_time.minute)
+
+        if working_start <= start_time and end_time <= working_end:
+            valid_slot = True
+            break
+
+    if not valid_slot:
+        raise serializers.ValidationError('The requested time slot is outside the provider\'s working hours.')
+
+    special_closures = provider.special_closures.filter(date=start_time.date())
+
+    if special_closures.exists():
+        raise serializers.ValidationError('The provider is closed this day.')
+
+    overlapping_slots = TimeSlot.objects.filter(
+        provider=provider,
+        is_available=False,
+        start_time__lt=end_time,
+        end_time__gt=start_time,
+    )
+
+    if overlapping_slots.exists():
+        raise serializers.ValidationError('The requested time slot is not available.')
