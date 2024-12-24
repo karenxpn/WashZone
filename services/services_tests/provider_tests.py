5a2a3f875d79ede2f6ca72906132c85af57fcde9
karenxpn <karenmirakyan@gmail.com>
2024-12-24 17:26:57 +0400

    provider tests file added

---
+++ services/services_tests/provider_tests.py
@@ -0,0 +1,6 @@
+from rest_framework.test import APITestCase, APIClient
+
+
+class ProviderViewSetTests(APITestCase):
+    def setUp(self):
+        pass
--- services/tests.py
+++ services/tests.py
@@ -2,3 +2,4 @@ from django.test import TestCase
 
 # Create your tests here.
 from services.services_tests.category_tests import *
+from services.services_tests.provider_tests import *
