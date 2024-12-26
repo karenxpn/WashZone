8eb5bdabf57c44ff751f1c96ce3bad630edc335b
karenxpn <karenmirakyan@gmail.com>
2024-12-26 13:15:50 +0400

    delete service tests

--- services/services_tests/service_tests.py
+++ services/services_tests/service_tests.py
@@ -117,6 +117,20 @@ class ServiceViewSetTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
+    # delete service tests
+    def test_delete_service_not_authenticated(self):
+        response = self.api_client.delete(reverse('service-detail', kwargs={'pk': self.service.pk}))
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_delete_service_authenticated(self):
+        self.api_client.force_authenticate(user=self.user)
+        response = self.api_client.delete(reverse('service-detail', kwargs={'pk': self.service.pk}))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_delete_service_not_owner(self):
+        self.api_client.force_authenticate(user=self.user2)
+        response = self.api_client.delete(reverse('service-detail', kwargs={'pk': self.service.pk}))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
 
