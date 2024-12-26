ea2d25f23502145ee3a0b38cfb182880043d1ef6
karenxpn <karenmirakyan@gmail.com>
2024-12-26 15:36:19 +0400

    create feature tests

--- services/services_tests/feature_tests.py
+++ services/services_tests/feature_tests.py
@@ -18,6 +18,14 @@ class FeatureViewSetTests(APITestCase):
             cost=2000
         )
 
+        self.valid_create_payload = {
+            'name': 'Feature',
+            'description': 'Feature description',
+            'cost': 2000,
+        }
+
+        self.invalid_create_payload = {}
+
         self.api_client = APIClient()
 
 
@@ -31,3 +39,18 @@ class FeatureViewSetTests(APITestCase):
         response = self.api_client.get(reverse('feature-list'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+
+    # create feature tests
+    def test_create_feature_not_authenticated(self):
+        response = self.api_client.post(reverse('feature-list'), data=self.valid_create_payload, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_feature_authenticated(self):
+        self.api_client.force_authenticate(user=self.user)
+        response = self.api_client.post(reverse('feature-list'), data=self.valid_create_payload, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_feature_invalid_payload(self):
+        self.api_client.force_authenticate(user=self.user)
+        response = self.api_client.post(reverse('feature-list'), data=self.invalid_create_payload, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
