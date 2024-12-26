5a082716eb9f53a3d5bb48d1a1604e79b647e5ad
karenxpn <karenmirakyan@gmail.com>
2024-12-27 01:20:22 +0400

    tests imported to global tests file of servives

--- services/tests.py
+++ services/tests.py
@@ -3,3 +3,5 @@ from django.test import TestCase
 # Create your tests here.
 from services.services_tests.category_tests import *
 from services.services_tests.provider_tests import *
+from services.services_tests.service_tests import *
+from services.services_tests.feature_tests import *
