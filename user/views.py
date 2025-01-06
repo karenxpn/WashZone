c19c20b9779cd73fa7e04a184936abca57e999ca
karenxpn <karenmirakyan@gmail.com>
2025-01-07 01:43:41 +0400

    generate presigned url global file created

--- user/views.py
+++ user/views.py
@@ -1,14 +1,13 @@
 from rest_framework import status
-from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from WashZone.presigned_url import generate_presigned_url
 from authentication.decorators import validate_request
 from user.schemas import user_schema, presigned_url_schema
 from user.serializers.update_user_serializer import UpdateUserSerializer
 from user.serializers.user_serializer import UserSerializer
-import boto3
 
 
 @user_schema
@@ -50,23 +49,4 @@ class PresignedURLView(APIView):
         file_name = request.data.get('file_name')
         file_type = request.data.get('file_type')
 
-        if not file_name or not file_type:
-            return Response({'message': 'Missing file_name or file_type'}, status=status.HTTP_400_BAD_REQUEST)
-
-        s3 = boto3.client('s3')  # to be added the region name
-        bucket_name = 'BucketName'  # to be added aws storage bucket name
-
-        try:
-            presigned_url = s3.generate_presigned_url(
-                'put_object',
-                Params={
-                    'Bucket': bucket_name,
-                    'Key': f'users/{file_name}',
-                    'ContentType': file_type
-                },
-                ExpiresIn=3600
-            )
-
-            return Response({'presigned_url': presigned_url}, status=status.HTTP_200_OK)
-        except Exception as e:
-            return Response({'message': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        return generate_presigned_url(file_name, file_type)
