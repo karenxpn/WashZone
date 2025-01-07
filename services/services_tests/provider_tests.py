1157241034f8b5b903181f4976202264357630a1
karenxpn <karenmirakyan@gmail.com>
2025-01-07 13:12:40 +0400

    log

--- services/services_tests/provider_tests.py
+++ services/services_tests/provider_tests.py
@@ -147,7 +147,7 @@ class ProviderViewSetTests(APITestCase):
         self.api_client.force_authenticate(user=self.user)
         response = self.api_client.post(reverse('provider-presigned-url'), data=self.presigned_url_valid_payload, format='json')
         print(response.data)
-        logging.log(response.data, msg='Log from response data')
+        logging.log(1, msg=f'The response data is {response.data}')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_get_presigned_url_invalid_payload(self):
