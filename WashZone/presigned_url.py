09ff89a5dc44a96ee9c7833735186f4e2f4e560c
karenxpn <karenmirakyan@gmail.com>
2025-01-10 21:37:52 +0400

    aws  credentials and presigned url generation fixes

--- WashZone/presigned_url.py
+++ WashZone/presigned_url.py
@@ -1,21 +1,29 @@
+import uuid
+
 import boto3
 from rest_framework import status
 from rest_framework.response import Response
+
+from WashZone import settings
 
 
 def generate_presigned_url(file_name, file_type, path):
     if not file_name or not file_type:
         return Response({'message': 'Missing file_name or file_type'}, status=status.HTTP_400_BAD_REQUEST)
 
-    s3 = boto3.client('s3')  # to be added the region name
-    bucket_name = 'BucketName'  # to be added aws storage bucket name
+    s3 = boto3.client('s3',
+                      aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+                      aws_secret_access_key=settings.AWS_SECRET_KEY,
+                      region_name='us-east-2'
+                      )
+    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
 
     try:
         presigned_url = s3.generate_presigned_url(
             'put_object',
             Params={
                 'Bucket': bucket_name,
-                'Key': f'{path}/{file_name}',
+                'Key': f'{path}/{uuid.uuid4()}_{file_name}',
                 'ContentType': file_type
             },
             ExpiresIn=3600
--- WashZone/settings.py
+++ WashZone/settings.py
@@ -211,7 +211,7 @@ SPECTACULAR_SETTINGS = {
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_SECRET_KEY = os.environ.get('AWS_SECRET_KEY')
 AWS_STORAGE_BUCKET_NAME = 'washzone'
 
 if os.getcwd() == '/app':
