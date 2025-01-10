f64db162848a04f69b1a11de74d9ac063e2c7d9b
karenxpn <karenmirakyan@gmail.com>
2025-01-10 22:04:03 +0400

    debug true

--- WashZone/settings.py
+++ WashZone/settings.py
@@ -213,6 +213,3 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_KEY = os.environ.get('AWS_SECRET_KEY')
 AWS_STORAGE_BUCKET_NAME = 'washzone'
-
-if os.getcwd() == '/app':
-    DEBUG = False
