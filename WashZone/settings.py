b6005cecf173a26b556ddc5cfe11f5c223b00b79
karenxpn <karenmirakyan@gmail.com>
2025-01-10 13:46:42 +0400

    whitenoise added

--- WashZone/settings.py
+++ WashZone/settings.py
@@ -69,6 +69,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'silk.middleware.SilkyMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'WashZone.urls'
