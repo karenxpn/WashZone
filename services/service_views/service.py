b0bf10fa803e6c7cc59e732a8bd942ff516b31b7
karenxpn <karenmirakyan@gmail.com>
2025-01-10 13:52:47 +0400

    sevice features endpoints updated to match restful rules

--- services/service_views/service.py
+++ services/service_views/service.py
@@ -65,17 +65,17 @@ class ServiceViewSet(viewsets.ModelViewSet):
             return Response({"message": "Service not found"}, status=status.HTTP_404_NOT_FOUND)
 
     @add_feature_schema
-    @action(detail=True, methods=['post'], url_path='add-feature')
+    @action(detail=True, methods=['post'], url_path='feature')
     def add_feature(self, request, pk=None):
         return add_feature_to_service(self, request)
 
     @remove_feature_schema
-    @action(detail=True, methods=['delete'], url_path=r'remove-feature/(?P<feature_id>\d+)')
+    @action(detail=True, methods=['delete'], url_path=r'feature/(?P<feature_id>\d+)')
     def remove_feature(self, request, pk=None, feature_id=None):
         return remove_feature(self, request, pk, feature_id)
 
     @update_feature_schema
-    @action(detail=True, methods=['patch'], url_path='update-feature')
+    @action(detail=True, methods=['patch'], url_path='feature')
     def update_feature(self, request, pk=None):
         return update_feature(self, request, pk)
 
